32a09b65c63bfa9056cbf58994b7c390c392c28f
siddhartha.kumar <siddhartha.kumar@mjpc965.metaljunction.com>
2017-09-12 14:20:24 +0530

    Updated DFS and added a class called Paths which woulf function as client ot DFS siddhartha.kumar

--- src/main/java/nonlinear/graph/api/DepthFirstSearch.java
+++ src/main/java/nonlinear/graph/api/DepthFirstSearch.java
@@ -1,16 +1,31 @@
 package nonlinear.graph.api;
 
 public class DepthFirstSearch {
-	
+
 	private DepthFirstSearchHelper[] depthFirstSearchHelperArr;
-	
-	public DepthFirstSearch(Graph G) {
+
+	private Graph G;
+
+	/**
+	 * 
+	 * @param G
+	 * @param source
+	 *            : the point at which the search has to be started
+	 */
+	public DepthFirstSearch(Graph G, int source) {
+		this.G = G;
 		this.depthFirstSearchHelperArr = new DepthFirstSearchHelper[G.noOfVertices()];
-		this.dfs();
+		this.dfs(source);
 	}
-	
-	private void dfs() {
-		
+
+	private void dfs(int source) {
+		this.depthFirstSearchHelperArr[source].setMarked(true);
+		G.adjacentVertices(source).forEach(vertex -> {
+			if (!this.depthFirstSearchHelperArr[(int) vertex].isMarked()) {
+				this.depthFirstSearchHelperArr[(int) vertex].setEdgeTo(source);
+				this.dfs((int)vertex);
+			}
+		});
 	}
-	
+
 }
---
+++ src/main/java/nonlinear/graph/client/Paths.java
@@ -0,0 +1,17 @@
+package nonlinear.graph.client;
+
+import java.util.List;
+
+public class Paths {
+
+	/***
+	 * 
+	 * @param v source vertex
+	 * @param w destination vertex
+	 * @return
+	 * This method returns a List of vertices connecting vertex v and vertex w
+	 */
+	public List<Integer> getPath(int v,int w){
+		return null;
+	}
+}

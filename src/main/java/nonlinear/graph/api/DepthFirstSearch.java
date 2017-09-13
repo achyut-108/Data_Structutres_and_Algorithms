29509b9b7f25a44359a21f12e8d94aef03e3ad49
siddhartha.kumar <siddhartha.kumar@mjpc965.metaljunction.com>
2017-09-13 17:56:48 +0530

    Added main method in Paths.
    siddhartha.kumar

--- src/main/java/nonlinear/graph/api/DepthFirstSearch.java
+++ src/main/java/nonlinear/graph/api/DepthFirstSearch.java
@@ -5,6 +5,10 @@ public class DepthFirstSearch {
 	private DepthFirstSearchHelper[] depthFirstSearchHelperArr;
 
 	private Graph G;
+	/***
+	 * The source vertex
+	 */
+	private int source;
 
 	/**
 	 * 
@@ -14,18 +18,35 @@ public class DepthFirstSearch {
 	 */
 	public DepthFirstSearch(Graph G, int source) {
 		this.G = G;
+		this.source = source;
 		this.depthFirstSearchHelperArr = new DepthFirstSearchHelper[G.noOfVertices()];
+		for(int i=0;i<G.noOfVertices();i++) {
+			this.depthFirstSearchHelperArr[i] = new DepthFirstSearchHelper();
+		}
 		this.dfs(source);
 	}
 
 	private void dfs(int source) {
+		System.out.println("[dfs] : depthFirstSearchHelperArr is : " + this.depthFirstSearchHelperArr);
 		this.depthFirstSearchHelperArr[source].setMarked(true);
 		G.adjacentVertices(source).forEach(vertex -> {
 			if (!this.depthFirstSearchHelperArr[(int) vertex].isMarked()) {
 				this.depthFirstSearchHelperArr[(int) vertex].setEdgeTo(source);
-				this.dfs((int)vertex);
+				this.dfs((int) vertex);
 			}
 		});
+	}
+
+	public DepthFirstSearchHelper[] getDepthFirstSearchHelperArr() {
+		return depthFirstSearchHelperArr;
+	}
+
+	public Graph getG() {
+		return G;
+	}
+
+	public int getSource() {
+		return source;
 	}
 
 }

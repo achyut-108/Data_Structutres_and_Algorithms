b98e4a2df3605c87018aca5bfa1ad8a99443dea1
siddhartha.kumar <siddhartha.kumar@mjpc965.metaljunction.com>
2017-09-22 18:31:58 +0530

    Coded Breadth First Search

--- src/main/java/nonlinear/graph/api/BreadthFirstSearch.java
+++ src/main/java/nonlinear/graph/api/BreadthFirstSearch.java
@@ -7,13 +7,26 @@ public class BreadthFirstSearch {
 
 	private BreadthFirstSearchHelper[] breadthFirstSearchHelperArr;
 	private Queue<Integer> queue = new LinkedBlockingQueue<>();
-	
-	public BreadthFirstSearch(Graph G,int source) {
+
+	public BreadthFirstSearch(Graph G, int source) {
+
+		breadthFirstSearchHelperArr = new BreadthFirstSearchHelper[G.noOfVertices()];
+		int temp_source = source;
 		queue.add(source);
-		while(!queue.isEmpty()) {
+		while (!queue.isEmpty()) {
 			int node = queue.poll();
-			
+			if (!breadthFirstSearchHelperArr[node].isMarked()) {
+				breadthFirstSearchHelperArr[node].setMarked(true);
+				breadthFirstSearchHelperArr[node].setEdgeTo(temp_source);
+			}
+			temp_source = node;
+			G.adjacentVertices(node).forEach(e -> {
+				queue.add((Integer) e);
+			});
 		}
 	}
-	
+
+	public static void main(String[] args) {
+		System.out.println();
+	}
 }

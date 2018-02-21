9714babf071ce2198dcb2596d042fff4a10966f3
siddhartha.kumar <siddhartha.kumar@mjpc965.metaljunction.com>
2018-02-21 18:08:15 +0530

    Added class for ConnectedComponents

---
+++ src/main/java/nonlinear/graph/api/ConnectedComponents.java
@@ -0,0 +1,23 @@
+package nonlinear.graph.api;
+
+
+// TO address is vertex 'v' connected to 'w'
+public class ConnectedComponents {
+	
+	private Graph G;
+	
+	public ConnectedComponents(Graph G) {
+		this.G = G;
+	}
+
+	public boolean isConnected(int v,int w) {
+		
+		return false;
+	}
+	
+	private void dfs() {
+		
+	}
+	
+}
+

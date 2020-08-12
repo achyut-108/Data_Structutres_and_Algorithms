b10f7d62e3304e56b4e2c6c5c6e89da82c8d9f14
sidkumar3 <sidkumar3@WKWIN6676092.global.publicisgroupe.net>
2020-08-12 13:59:37 +0530

    Majority class

--- src/main/java/interviewbit/interview/questions/microsift/MajorityElement.java
+++ src/main/java/interviewbit/interview/questions/microsift/MajorityElement.java
@@ -1,5 +1,45 @@
 package interviewbit.interview.questions.microsift;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Objects;
+
 public class MajorityElement {
+
+	public static Integer compute(List<Integer> list) {
+
+		if (Objects.isNull(list))
+			return null;
+
+		Map<Integer, Integer> countMap = new HashMap<Integer, Integer>();
+
+		list.parallelStream().forEach(integer -> {
+			Integer count = countMap.get(integer);
+			if (count != null)
+				countMap.put(integer, ++count);
+			else
+				countMap.put(integer, 1);
+
+		});
+
+		Integer majorityElement = null;
+		Integer countOfMajorityElement = 0;
+
+		for (Entry<Integer, Integer> entrySet : countMap.entrySet()) {
+			if (countOfMajorityElement < entrySet.getValue()) {
+				majorityElement = entrySet.getKey();
+				countOfMajorityElement = entrySet.getValue();
+			}
+		}
+		return majorityElement;
+	}
+
+	public static void main(String[] args) {
+		Integer[] arr = { 12 };
+		System.out.println("result : " + compute(Arrays.asList(arr)));
+	}
 
 }
